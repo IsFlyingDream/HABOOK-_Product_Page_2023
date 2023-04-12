f5452f643c06ba78a4db37820fd5b13cf060df15
ruby <ruby29922026@gmail.com>
2023-04-12 11:24:31 +0800

    將購物明細加入Vue，並且加入json檔做測試

--- js/MyScript.js
+++ js/MyScript.js
@@ -1,5 +1,74 @@
+const { createApp, onBeforeMount } = Vue;
+createApp({
+	data() {
+		return {
+			orders: [
+				//{訂單編號number:'',付款時間paytime:'',付款金額pay:'',付款方式way:'',購買項目item:'',序號與型號key:''},		
+				{ number: 'abcccc', paytime: '2020/06/30 15:37:49', pay: 1000, way: '轉帳', item: '醍摩豆雲平台IES 5', key: 'abcabc' },
+				{ number: 'caaba', paytime: '2020/06/30 15:37:49', pay: 1000, way: '轉帳', item: '醍摩豆雲平台IES 5', key: 'abcabc' },
+			],
+		}
+	},
+	methods: {
+		listBtn() {//上方菜單顯示與隱藏的按鈕↓↓
+			var listBtn = document.getElementById('listBtn');
+			var textlistn = document.getElementById('HidAndShow');
+			var menu = document.getElementById('menu');
+			if (textlistn.style.display === "block") {
+				HidAndShow.style.display = "none";
+				menu.style['height'] = "80px";
+			} else {
+				HidAndShow.style.display = "block";
+				menu.style['height'] = "auto";
+			}
+		},
+		fetchData() {
+			console.log("fetchData in");
+			//axios.get('https://cdn.jsdelivr.net/gh/IsFlyingDream/test_project/API_response/gcb.json')
+			axios.get('./jsonTest.json')
+				.then(response => {
+					console.log("fetchData2 in");
+					//this.orders = response.data.orders;
+					//this.agentPassStat_passed = response.data.results.agentPassStat.passed;
+					//console.log("orders="+this.orders);
 
-document.write('<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js" type="text/javascript" charset="utf-8"></script>'); //注意,此處須為相對於index.html的絕對路徑
+				})
+				.catch(error => {
+					console.log(error)
+				});
+		},
+		mouseOver() {//滑鼠移入上方菜單↓↓
+			var mouseFunction = document.getElementById('mouseFunction');
+			mouseFunction.style['background-color'] = "rgba(0, 0, 0, .6)";
+			mouseFunction.style['transition'] = "background-color 0.4s";
+		},
+		mouseOut() {
+			var mouseFunction = document.getElementById('mouseFunction');
+			mouseFunction.style['background-color'] = "";
+		},
+	},
+	mounted() {
+		this.fetchData();
+	}
+}).mount('#VueApp');
+
+
+//上方菜單顯示與隱藏的按鈕↓↓
+window.onresize = function () {  //每次視窗改變都會載入下列事件
+	var listBtn = document.getElementById('listBtn');
+	// var textlistn = document.getElementById('HidAndShow');
+	if (document.body.scrollWidth >= 610) {
+		listBtn.style.display = "none";
+		HidAndShow.style.display = "block";
+	} else {
+		listBtn.style.display = "block";
+		HidAndShow.style.display = "none";
+	}
+};
+
+//上方菜單顯示與隱藏的按鈕↑↑-----------------------------
+
+
 
 
 //為傳統js用法↓↓----------------------------------------------------------------------------------------------------
@@ -18,177 +87,6 @@ document.write('<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js" type=
 }
 //讓上方菜單frameid大小自動改變↑↑
 
-//上方菜單顯示與隱藏的按鈕↓↓
-
-function listBtn() {
-	var listBtn = document.getElementById('listBtn');
-	var textlistn = document.getElementById('HidAndShow');
-	if (textlistn.style.display === "block") {
-		HidAndShow.style.display = "none";
-	} else {
-		HidAndShow.style.display = "block";
-	}
-}
-function listBtnlistBtn() {
-	var listBtn = document.getElementById('listBtn');
-	var textlistn = document.getElementById('HidAndShow');
-	if (textlistn.style.display === "block") {
-		HidAndShow.style.display = "none";
-		var mouseFunction = document.getElementById('mouseFunction');
-		mouseFunction.style['background-color'] = "";
-	} else {
-		HidAndShow.style.display = "block";
-		var mouseFunction = document.getElementById('mouseFunction');
-		mouseFunction.style['background-color'] = "rgba(0, 0, 0, .6)";
-		mouseFunction.style['transition'] = "background-color 0.4s";
-	}
-}
-
-window.onresize = function () {  //每次視窗改變都會載入下列事件
-	var listBtn = document.getElementById('listBtn');
-	var textlistn = document.getElementById('HidAndShow');
-	if (document.body.scrollWidth >= 870) {
-		listBtn.style.display = "none";
-		HidAndShow.style.display = "block";
-	} else {
-		listBtn.style.display = "block";
-		HidAndShow.style.display = "none";
-	}
-};
-
-//上方菜單顯示與隱藏的按鈕↑↑
-
-
-//滑鼠移入上方菜單↓↓
-function mouseOver() {
-	var mouseFunction = document.getElementById('mouseFunction');
-	mouseFunction.style['background-color'] = "rgba(0, 0, 0, .6)";
-	mouseFunction.style['transition'] = "background-color 0.4s";
-}
-function mouseOut() {
-	var mouseFunction = document.getElementById('mouseFunction');
-	mouseFunction.style['background-color'] = "";
-}
-
-//滑鼠移入上方菜單↑↑
 //為傳統js用法↑↑----------------------------------------------------------------------------------------------------------------*/
 
 // 以下改成Vue用法----------------------------------------------------------------------------------------------------------------
-
-
-const vm = new Vue({
-	el: '#VueApp',		
-	data:{
-		portfolios:[
-			{num:1,src:'https://i.imgur.com/tsPjIhJ.png',png:'https://i.imgur.com/tsPjIhJ.png',gif:'https://i.imgur.com/KrBGi8T.mp4'},			
-			{num:2,src:'https://i.imgur.com/qCZtBcJ.png',png:'https://i.imgur.com/qCZtBcJ.png',gif:'https://i.imgur.com/un3NtiC.mp4'},
-			{num:3,src:'https://i.imgur.com/iLJTohD.png',png:'https://i.imgur.com/iLJTohD.png',gif:'https://i.imgur.com/u07gx7B.mp4'},
-			{num:4,src:'https://i.imgur.com/VznWKE7.png',png:'https://i.imgur.com/VznWKE7.png',gif:'https://i.imgur.com/lcSetmt.mp4'},
-			{num:5,src:'https://i.imgur.com/AeVqgpr.png',png:'https://i.imgur.com/hNj42v7.png',gif:'https://i.imgur.com/6Ch2Q5V.mp4'},
-		],
-	},
-	methods: {
-		listBtn() {//上方菜單顯示與隱藏的按鈕↓↓
-			var listBtn = document.getElementById('listBtn');
-			var textlistn = document.getElementById('HidAndShow');
-			var menu = document.getElementById('menu');
-			if (textlistn.style.display === "block") {
-				HidAndShow.style.display = "none";
-				menu.style['height'] = "80px";
-			} else {
-				HidAndShow.style.display = "block";				
-				menu.style['height'] = "auto";
-			}
-		},
-		// IndexlistBtn() {
-		// 	var textlistn = document.getElementById('HidAndShow');
-		// 	if (textlistn.style.display === "block") {
-		// 		HidAndShow.style.display = "none";
-		// 		var mouseFunction = document.getElementById('mouseFunction');
-		// 		//mouseFunction.style['background-color'] = "";
-		// 	} else {
-		// 		HidAndShow.style.display = "block";
-		// 		var mouseFunction = document.getElementById('mouseFunction');
-		// 		//mouseFunction.style['background-color'] = "rgba(0, 0, 0, .6)";
-		// 		//mouseFunction.style['transition'] = "background-color 0.4s";
-		// 	}
-		// },//上方菜單顯示與隱藏的按鈕↑↑
-
-		mouseOver() {//滑鼠移入上方菜單↓↓
-			var mouseFunction = document.getElementById('mouseFunction');
-			mouseFunction.style['background-color'] = "rgba(0, 0, 0, .6)";
-			mouseFunction.style['transition'] = "background-color 0.4s";
-		},
-		mouseOut() {
-			var mouseFunction = document.getElementById('mouseFunction');
-			mouseFunction.style['background-color'] = "";
-		},
-		imgplay(num){//作品集頁面，點擊則會換成動圖		//因為gif太大，放到IMG會轉成影片，就不使用這功能了。	
-			let tempStr = this.portfolios[num].src;
-			let types = tempStr.substr(tempStr.length-3,3);
-			if(types=="png"){
-				this.portfolios[num].src = this.portfolios[num].gif;
-			}else{
-				this.portfolios[num].src = this.portfolios[num].png;
-			}
-		},
-	},
-});
-
-
-//上方菜單顯示與隱藏的按鈕↓↓
-window.onresize = function () {  //每次視窗改變都會載入下列事件
-	var listBtn = document.getElementById('listBtn');
-	var textlistn = document.getElementById('HidAndShow');
-	if (document.body.scrollWidth >= 610) {
-		listBtn.style.display = "none";
-		HidAndShow.style.display = "block";
-	} else {
-		listBtn.style.display = "block";
-		HidAndShow.style.display = "none";
-	}
-};
-
-//上方菜單顯示與隱藏的按鈕↑↑-----------------------------
-
-//實現輪播全頻↓↓-----------------------------
-var $item = $('.carousel-item');
-var $wHeight = $(window).height();
- 
-$item.height($wHeight);
-$item.addClass('full-screen');
- 
-$('.carousel img').each(function() {
-  var $src = $(this).attr('src');
-  var $color = $(this).attr('data-color');
-  $(this).parent().css({
-    'background-image' : 'url(' + $src + ')',
-    'background-color' : $color
-  });
-  $(this).remove();
-});
- 
-$(window).on('resize', function (){
-  $wHeight = $(window).height();
-  $item.height($wHeight);
-});
-//實現輪播全頻↑↑-----------------------------
-
-window.addEventListener('resize', function () {
-    var img = document.querySelectorAll('#carouselExampleCaptions .carousel-item img');
-    var height = window.innerHeight;
-    for (var i = 0; i < img.length; i++) {
-      var width = img[i].naturalWidth;
-      var ratio = height / img[i].naturalHeight;
-      img[i].style.height = height + 'px';
-      img[i].style.width = width * ratio + 'px';
-    }
-  });
-  window.dispatchEvent(new Event('resize'));
-
-//改變輪播時間↓↓ 5000=5秒-----------------------------
-$('.carousel').carousel({
-	interval: 5000
-  });
-//改變輪播時間↑↑-----------------------------
-
